c08a1a0037923be2054fd59c66ca765d57aad9c4
Angelo Geulin <angelogeulin123@gmail.com>
2023-07-04 08:38:54 +0800

    add code that causes the bug

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,12 @@
+import { Inter } from "next/font/google"
+const inter = Inter({ subsets: ["latin"] })
+
 export default function RootLayout({ children }) {
   return (
     <html>
       <head />
-      <body>{children}</body>
+      {/* Flickering occurs when you add this next/font `style` attribute. */}
+      <body style={inter.style}>{children}</body>
     </html>
   )
 }
